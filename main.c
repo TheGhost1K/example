ab3b876665e06bf124a7f399202167a97fdbb0a8
theghost <Losckutow.maxim@yandex.ru>
2020-10-21 15:18:35 +0500

    rewrite

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-#include "askname.h"
-
 int main(int argc, char **argv)
 {
-    char first[255], last[255];
-
-    askname(first, last);
-
-    printf("Hello dear, %s %s!\n", first, last);
+    printf("Hello");
     return 0;
 }
 
